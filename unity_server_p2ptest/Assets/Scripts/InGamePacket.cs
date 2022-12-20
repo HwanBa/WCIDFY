7ef285c7fba7c6a62ac9779597cd9d769ae3ffec
Michael_Kim <un8265@gmail.com>
2022-12-20 17:52:17 +0900

    Set gitignore

---
+++ unity_server_p2ptest/Assets/Scripts/InGamePacket.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InGamePacket
+{
+    // - InGamePacketType
+    public enum PacketType
+    {
+        PACKET_TYPE_NONE,
+        PACKET_TYPE_END
+    }
+}
--- unity_server_p2ptest/Assets/Scripts/PTPServerManager.cs
+++ unity_server_p2ptest/Assets/Scripts/PTPServerManager.cs
@@ -4,7 +4,8 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Text;
 using UnityEngine;
-using Packet;
+using InGamePacket;
+using Unity.VisualScripting;
 
 public class PTPServerManager : Singleton<PTPServerManager>
 {
@@ -18,5 +19,52 @@ public class PTPServerManager : Singleton<PTPServerManager>
     private Thread          _thread     = null;
 
     private byte[]          _buffer     = new byte[PACKET_MAX];
-    
+    private int             _packetsize = 0;
+    private int             _readsize = 0;
+    private PacketType      _packettype = PacketType.PACKET_TYPE_NONE;
+
+    public delegate void PacketHandler(in byte[] inbuffer);
+    private Dictionary<PacketType, PacketHandler> _packethandlermap = new Dictionary<PacketType, PacketHandler>();
+
+    struct MessageInfo
+    {
+        public PacketType type;
+        public byte[] buffer;
+    }
+    private List<MessageInfo> _messagequeue = new List<MessageInfo>();
+
+    private void Update()
+    {
+        ExecuteMessage();
+    }
+
+    private void ExecuteMessage()
+    {
+        if (_messagequeue.Count == 0)
+            return;
+
+        MessageInfo msg = _messagequeue[0];
+        if (!_packethandlermap.ContainsKey(msg.type))
+            return;
+
+        _packethandlermap[msg.type](msg.buffer);
+    }
+
+    // - If Application Quit, Memory Release
+    private void OnApplicationQuit()
+    {
+        Release();
+    }
+
+    private void Release()
+    {
+        if (_tcpclient != null)
+            _tcpclient.Close();
+
+        if (_netstream != null)
+            _netstream.Close();
+
+        if (_thread != null)
+            _thread.Abort();
+    }
 }
